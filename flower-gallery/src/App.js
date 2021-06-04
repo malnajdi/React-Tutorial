4ffd724899bf22b684cd57a6705859f80ca1b18d
Mohammed Y. Alnajdi <mohdnagfy@gmail.com>
2021-06-04 21:57:13 +0300

    الدرس الثالث

--- flower-gallery/src/App.js
+++ flower-gallery/src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header firstName="1" lastName="3" />
+      <Header firstName="2" lastName="2" />
+      <Header firstName="3" lastName="1" />
     </div>
   );
 }
--- flower-gallery/src/components/header/Header.js
+++ flower-gallery/src/components/header/Header.js
@@ -1,9 +1,20 @@
+import React from 'react'
 import './Header.css';
 
-function Header () {
+// [1] : Function Based Component
+function Header (props) {
     return (
-        <h2>Header</h2>
+        <h2>Welcome {props.firstName} {props.lastName}</h2>
     );
 }
+
+// [2] : Class Based Component
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <h2>Class Based Header</h2>
+//         );
+//     }
+// }
 
 export default Header;
